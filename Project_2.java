04a4c8202877b0a06a6968240c89b2cc295e6fb5
janvi <janvidagar777@gmail.com>
2025-08-23 18:06:59 +0530

    Initial commit

---
+++ Project_2.java
@@ -0,0 +1,29 @@
+package myProjects;
+import java.util.Scanner;
+import java.util.Random;
+public class Project_2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        Random ran=new Random();
+        int rnm=ran.nextInt(100)+1;
+        System.out.println("hey! this is number guessing game");
+        System.out.println("you have to choose one number between 1 and 100");
+       int guess=-1;
+        int attempts=0;
+        while(rnm!=guess){
+            System.out.println("enter your number");
+            guess=s.nextInt();
+            attempts++;
+            if (guess>rnm) {
+                System.out.println("too high! enter low number");
+            }
+            else if(guess<rnm){
+                System.out.println("too low! enter high number");
+            }
+            else{
+                System.out.println("great! you did it in "+attempts+"attempts");
+            }
+        }
+
+    }
+}
