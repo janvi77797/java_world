efb0013f902beedd49dedcff4d827cad2e785a8b
Janvi Dagar <janvidagar777@gmail.com>
2025-09-26 18:27:51 +0530

    Add files via upload

---
+++ Project_3.java
@@ -0,0 +1,53 @@
+package myProjects;
+
+class Library{
+    String [] books;
+    int No_of_books;
+     Library(){
+        this.books=new String[50];
+        this.No_of_books=0;
+    }
+    void addBook(String book){
+         this.books[No_of_books]=book;
+         No_of_books++;
+        System.out.println(book+" has been added!");
+    }
+    void showAvailableBooks(){
+        System.out.println("Available books are:");
+         for(String book: this.books){
+             if(book==null){
+                 continue;
+             }
+             System.out.println("*"+book);
+         }
+    }
+    void issueBook(String book){
+         for(int i=0; i<this.books.length; i++){
+             if(this.books[i] != null && this.books[i].equals(book)){
+                 System.out.println(book+" has been issued");
+                 this.books[i]=null;
+                 return;
+             }
+         }
+        System.out.println("book is not available");
+    }
+    void returnBook(String book){
+         addBook(book);
+    }
+}
+public class ForPractise {
+    public static void main(String[] args) {
+        Library l=new Library();
+
+     l.addBook("dsa");
+     l.addBook("c++");
+     l.addBook("java");
+     l.addBook("sql");
+     l.addBook("linux");
+     l.showAvailableBooks();
+        l.issueBook("sql");
+        l.showAvailableBooks();
+        l.returnBook("sql");
+        l.showAvailableBooks();
+        }
+    }
